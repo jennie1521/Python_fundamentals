1e764a2c3ac2b232ff3bb9050c4af75a2a644d9d
Janvi Yadav <janvi1552005@gmail.com>
2026-07-21 21:09:08 +0530

    Update queue_stack.py

--- queue_stack.py
+++ queue_stack.py
@@ -16,3 +16,20 @@ q.enqueue(30)
 print(q.values)
 print(q.dequeue())
 print(q.values)
+
+#stack
+class Stack:
+    def __init__(self):
+        self.values=[]
+    def push(self,x):
+        self.values=[x]+self.values
+    def pop(self):
+        return self.values.pop(0)
+s=Stack()
+s.push(10)
+s.push(20)
+s.push(30)
+s.push(50)
+print(s.values)
+s.pop()
+print(s.values)
